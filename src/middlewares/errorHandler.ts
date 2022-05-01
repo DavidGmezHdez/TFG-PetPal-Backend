5c7a941f15b2f054e14aeaf1bf51e4b33d646691
DavidGmezHdez <davidgmezhdez@gmail.com>
2022-05-01 17:08:58 +0200

    Fixing error handling lint

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,14 +1,9 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import httpStatus from "http-status";
 import { ApplicationError, NotFoundError } from "../utils/errors";
 import logger from "../utils/logger";
 
-export const handleError = (
-    err: Error,
-    _req: Request,
-    res: Response,
-    _next: NextFunction
-) => {
+export const handleError = (err: Error, _req: Request, res: Response) => {
     if (err instanceof ApplicationError) {
         logger.error(err.statusCode, err.stack);
         if (err instanceof NotFoundError) {
