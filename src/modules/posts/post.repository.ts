c1e22f097bffd4a2b8334b05ca69a95c5825a8ff
DavidGmezHdez <davidgmezhdez@gmail.com>
2022-06-25 15:38:05 +0200

    Dont changing user

--- src/modules/posts/post.repository.ts
+++ src/modules/posts/post.repository.ts
@@ -17,10 +17,6 @@ export default class PostRepository {
 
     static async create(post) {
         const createdPost = await PostModel.create(post);
-        await UserModel.updateOne(
-            { _id: post.author },
-            { $push: { posts: createdPost._id } }
-        );
         return createdPost;
     }
 
@@ -46,12 +42,8 @@ export default class PostRepository {
 
     static async destroy(id: string) {
         const deletedPost = await PostModel.findByIdAndDelete(id);
+        console.log(deletedPost);
         if (!deletedPost) throw new NotFoundError(`Post doesn't exist`);
-        await UserModel.updateOne(
-            { _id: deletedPost.author },
-            { $pull: { posts: deletedPost._id } },
-            { safe: true, multi: false }
-        );
         return deletedPost;
     }
 }
